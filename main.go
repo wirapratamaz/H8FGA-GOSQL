ac5b392f78d0bee4f6b378aba28ab2ff90d5a177
wirapratamaz <wira@gringgo.co>
2023-03-30 22:59:59 +0800

    create data

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "admin"
+	dbname   = "db-go-sql"
+)
+
+var (
+	db  *sql.DB
+	err error
+)
+
+type Employee struct {
+	ID        int
+	Full_name string
+	Email     string
+	Age       int
+	Division  string
+}
+
+func CreateEmployee() {
+	var employee = Employee{}
+
+	sqlStatement := `
+	INSERT INTO employees (full_name, email, age, division)
+	VALUES ($1, $2, $3, $4)
+	RETURNING *
+	`
+
+	err = db.QueryRow(sqlStatement, "Ariel", "ariel@mail.com", 23, "IT").
+		Scan(&employee.ID, &employee.Full_name, &employee.Email, &employee.Age, &employee.Division)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("New Employee Data: %v\n", employee)
+}
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to database")
+
+	//CREATE DATA
+	CreateEmployee()
+}
